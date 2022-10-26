9680b62088d2d03790a7106a043fafb56ab4e263
David Murillo <david.murillo@ascendle.com>
2022-10-25 21:42:37 -0500

    tests

--- src/tests/components/GiftItem.test.jsx
+++ src/tests/components/GiftItem.test.jsx
@@ -1,10 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GiftItem } from "../../components";
 
 describe('Test GiftItem', () => {
+	const url = 'urlTest';
+	const titulo = 'tituloTest';
+
 	test('debe hacer match con el snapshot', () => {
-		const { container } = render(<GiftItem titulo='tituloTest' url='urlTest' />);
+		const { container } = render(<GiftItem titulo={titulo} url={url} />);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	test('debe mostrar la imagen con el URL y ALT indicado', () => {
+		render(<GiftItem titulo={titulo} url={url} />);
+		const { src, alt } = screen.getByRole('img');
+
+		expect(src).toContain(url);
+		expect(alt).toBe(titulo);
 	});
 });
