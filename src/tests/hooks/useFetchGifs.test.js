428d8a024e5b71724232614ec6260a9c0b5a849e
David Murillo <david.murillo@ascendle.com>
2022-11-01 18:28:59 -0500

    tests

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,9 +1,24 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('probar useFetchGifs', () => {
 	test('debe retornar el estado inicial', () => {
-		renderHook(() => { useFetchGifs('One Punch') });
+		const { result } = renderHook(() => useFetchGifs('One Punch'));
+		const { images, loading } = result.current;
 
+		expect(images.length).toBe(0);
+		expect(loading).toBeTruthy();
+	});
+
+	test('debe retornar un arreglo de imagenes y loading en false', async () => {
+		const { result } = renderHook(() => useFetchGifs('One Punch'));
+
+		await waitFor(() => {
+			expect(result.current.images.length).toBeGreaterThan(0);
+		});
+		const { images, loading } = result.current;
+
+		expect(images.length).toBeGreaterThan(0);
+		expect(loading).toBeFalsy();
 	});
 })
